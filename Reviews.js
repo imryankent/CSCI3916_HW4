114947ca564cc2135f7783f645ac9ce95e799e29
imryankent <ryan.kent@ucdenver.edu>
2021-05-12 14:08:00 -0600

    ReviewSchema finished

---
+++ Reviews.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.Promise = global.Promise;
+
+//mongoose.connect(process.env.DB, { useNewUrlParser: true });
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error) {
+    console.log("could not connect");
+}
+mongoose.set('useCreateIndex', true);
+
+//review schema
+var ReviewSchema = new Schema({
+    name: {type: string, required: true},
+    quote: {type: string},
+    stars: {type: Number, min: 1, max: 5},
+    title: {type: String, required: true},
+});
+
+
+//return the model to server
+module.exports = mongoose.model('Review', ReviewSchema);
